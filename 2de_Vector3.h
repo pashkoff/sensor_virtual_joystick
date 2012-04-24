c99b52ef23593b6d56d6c025e6620e7b5804af89
pashkoff <pashkoff2@gmail.com>
2012-04-25 00:02:25 +0300

    vector3 uses double

--- 2de_Vector3.h
+++ 2de_Vector3.h
@@ -1,18 +1,22 @@
 #ifndef _2DE_VECTOR3_H_
 #define _2DE_VECTOR3_H_
 
+#include <cassert>
+
 //#include "2de_Math.h"
+
 
 #define __INLINE inline
 
 class Vector3
 {
 public:
-	float x, y, z;
+	typedef double value_type;
+	value_type x, y, z;
 
-	__INLINE Vector3(float Ax = 0.0f, float Ay = 0.0f, float Az = 0.0f) : x(Ax), y(Ay), z(Az){}
+	__INLINE Vector3(value_type Ax = 0.0f, value_type Ay = 0.0f, value_type Az = 0.0f) : x(Ax), y(Ay), z(Az){}
 
-	__INLINE float operator[](int i)
+	__INLINE value_type operator[](int i)
 	{
 		assert(i >= 0 && i <= 2);
 		return *(&x + i);
@@ -28,29 +32,29 @@ public:
 		return Vector3(x - v.x, y - v.y, z - v.z);
 	}
 
-	__INLINE Vector3 operator +(float s) const
+	__INLINE Vector3 operator +(value_type s) const
 	{
 		return Vector3(x + s, y + s, z + s);
 	}
 
-	__INLINE Vector3 operator -(float s) const
+	__INLINE Vector3 operator -(value_type s) const
 	{
 		return Vector3(x - s, y - s, z - s);
 	}
 
-	__INLINE Vector3 operator *(float s) const
+	__INLINE Vector3 operator *(value_type s) const
 	{
 		return Vector3(x * s, y * s, z * s);
 	}
 
-	__INLINE Vector3 operator /(float s) const
+	__INLINE Vector3 operator /(value_type s) const
 	{
 		assert(s != 0.0f);
-		float t = 1.0f / s;
+		value_type t = 1.0f / s;
 		return Vector3(x * t, y * t, z * t);
 	}
 
-	friend Vector3 operator *(float s, const Vector3 &v)
+	friend Vector3 operator *(value_type s, const Vector3 &v)
 	{
 		return Vector3(v.x * s, v.y * s, v.z * s);
 	}
@@ -89,7 +93,7 @@ public:
 		return Vector3(-x, -y, -z);
 	}
 
-	__INLINE float operator *(const Vector3 &v) const
+	__INLINE value_type operator *(const Vector3 &v) const
 	{
 		return x * v.x + y * v.y + z * v.z;
 	}
@@ -99,28 +103,28 @@ public:
 		return Vector3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
 	}
 
-	__INLINE float Length() const
+	__INLINE value_type Length() const
 	{
-		return static_cast<float>(sqrt(static_cast<double>(x * x + y * y + z * z)));
+		return static_cast<value_type>(sqrt(static_cast<value_type>(x * x + y * y + z * z)));
 	}
 
-	float Normalize()
+	value_type Normalize()
 	{
-		float l = Length();
-		assert(l != 0.0f);
-		(*this) *= (1.0f / l);
+		value_type l = Length();
+		assert(l != 0.0);
+		(*this) *= (1.0 / l);
 		return l;
 	}
 
 	Vector3 Normalized()
 	{
-		float l = Length();
+		value_type l = Length();
 		assert(l != 0.0f);
 		Vector3 t = (*this) * (1.0f / l);
 		return t;
 	}
 
-	float ComputeNormal(const Vector3& V0, const Vector3& V1, const Vector3& V2)
+	value_type ComputeNormal(const Vector3& V0, const Vector3& V1, const Vector3& V2)
 	{
 		Vector3 E = V1 - V0;
 		Vector3 F = V2 - V1;
